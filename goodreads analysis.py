84bb86e98877601b60cba89548082b170e805156
causticwonder <112646366+causticwonder@users.noreply.github.com>
2022-09-18 09:59:24 -0400

    Update goodreads analysis.py

--- goodreads analysis.py
+++ goodreads analysis.py
@@ -1,1 +1,7 @@
 import json
+
+#open json data into read mode - first key is userdata and will throw an error code otherwise
+with open(r'C:\Users\dange\Desktop\Goodreads-Analysis_Code-Lou\track_book_origins.json') as f:
+    posts_json = json.load(f)
+
+type(posts_json)
