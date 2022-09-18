09b7910cec4db9263124b572e386c56326494e79
causticwonder <112646366+causticwonder@users.noreply.github.com>
2022-09-18 19:45:07 -0400

    3082 items in list

--- goodreads analysis.py
+++ goodreads analysis.py
@@ -1,10 +1,11 @@
 import json
 
-file_name = 'data/track_book_origins.json'
 #open json data into read mode - first key is userdata and will throw an error code otherwise
-with open(file_name, 'r', encoding="utf8") as f:
+with open('data/track_book_origins_edit.json', 'r') as f:
     json_data = json.load(f)
 
-#check data type
 print(type(json_data))
+
+print(len(json_data))
+
 
