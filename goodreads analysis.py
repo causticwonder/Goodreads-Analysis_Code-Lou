d3854ba4d546557bb8612a9dbd8320655211c25d
causticwonder <112646366+causticwonder@users.noreply.github.com>
2022-10-05 13:18:34 -0400

    import data as dictionary, clean up json files

--- goodreads analysis.py
+++ goodreads analysis.py
@@ -1,11 +1,18 @@
 import json
 
 #open json data into read mode - first key is userdata and will throw an error code otherwise
-with open('data/track_book_origins_edit.json', 'r') as f:
-    json_data = json.load(f)
+with open('data/track_book_origins_edit.json', 'r') as json_file:
+    json_data = json.load(json_file)
 
 print(type(json_data))
 
 print(len(json_data))
 
 
+#open json data into read mode - first key is userdata and will throw an error code otherwise
+with open('data/activity_edit.json', 'r') as f:
+    json_data_activity = json.load(f)
+
+print(type(json_data_activity))
+
+print(len(json_data_activity))
